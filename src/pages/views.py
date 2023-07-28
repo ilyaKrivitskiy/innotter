44db052dfb1ce28dfc782eedb878d8cf48927425
ilyaKrivitskiy <krivitskiy-ilya@mail.ru>
2023-07-28 15:07:46 +0300

    added simple crud with viewsets

--- src/pages/views.py
+++ src/pages/views.py
@@ -1,20 +1,22 @@
 from django.shortcuts import render
 from rest_framework import viewsets
+from django.shortcuts import get_object_or_404
 from .serializers import PageSerializer, TagSerializer, PostSerializer
 from .models import Page, Tag, Post
+from rest_framework import mixins
 from rest_framework.response import Response
 
 
-class PageViewSet(viewsets.ViewSet):
+class PageViewSet(viewsets.ModelViewSet):
     serializer_class = PageSerializer
     queryset = Page.objects.all()
 
 
-class TagViewSet(viewsets.ViewSet):
+class TagViewSet(viewsets.ModelViewSet):
     serializer_class = PageSerializer
     queryset = Tag.objects.all()
 
 
-class PostViewSet(viewsets.ViewSet):
+class PostViewSet(viewsets.ModelViewSet):
     serializer_class = PostSerializer
     queryset = Tag.objects.all()
--- src/users/views.py
+++ src/users/views.py
@@ -6,39 +6,6 @@ from .models import User
 from rest_framework.response import Response
 
 
-class UserViewSet(viewsets.ViewSet):
+class UserViewSet(viewsets.ModelViewSet):
     serializer_class = UserSerializer
     queryset = User.objects.all()
-
-    def list(self, request):
-        serializer = self.serializer_class(self.queryset, many=True)
-        return Response(serializer.data)
-
-    def retrieve(self, request, pk=None):
-        user = get_object_or_404(self.queryset, pk=pk)
-        serializer = self.serializer_class(user)
-        return Response(serializer.data)
-
-    def create(self, request):
-        serializer = UserSignUpSerializer(data=request.data)
-        serializer.is_valid(raise_exception=True)
-        serializer.save()
-        return Response({"message": "User Created Successfully."})
-
-    def update(self, request, pk=None):
-        user = get_object_or_404(self.queryset, pk=pk)
-        serializer = self.serializer_class(user, data=request.data)
-        serializer.is_valid(raise_exception=True)
-        serializer.save()
-        return Response(serializer.data)
-
-    def partial_update(self, request, pk=None):
-        user = get_object_or_404(self.queryset, pk=pk)
-        serializer = self.serializer_class(user, data=request.data, partial=True)
-        serializer.is_valid(raise_exception=True)
-        serializer.save()
-        return Response(serializer.data)
-
-    def destroy(self, request, pk=None):
-        self.queryset.filter(pk=pk).delete()
-        return Response("")
