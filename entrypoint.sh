6755eeed426e6a1c2d98cff0a2dc3bcf406ba235
ilyaKrivitskiy <krivitskiy-ilya@mail.ru>
2024-08-16 10:24:35 +0300

    created homepage; issues fixed

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,5 +1,16 @@
 #!/bin/bash -x
 
+if [ "$DATABASE" = "postgres" ]
+then
+    echo "Waiting for postgres..."
+
+    while ! nc -z $DB_HOST $DB_PORT; do
+      sleep 0.1
+    done
+
+    echo "PostgreSQL started"
+fi
+
 poetry run python3 ./src/manage.py migrate --noinput
 poetry run python3 ./src/manage.py runserver 0.0.0.0:8080
 
